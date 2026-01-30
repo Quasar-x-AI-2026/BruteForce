f43b4e1de1ec64c496e7c7e58a131e94851ee9f1
Prakash Kumar Rai <kumarprakash999721@gmail.com>
2026-01-30 16:47:53 +0530

    bit of css

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./app"
 import { AuthProvider } from "./auth/authcontext"
-//import "./index.css"
+import "./index.css"
+import "./loginpage.css"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
